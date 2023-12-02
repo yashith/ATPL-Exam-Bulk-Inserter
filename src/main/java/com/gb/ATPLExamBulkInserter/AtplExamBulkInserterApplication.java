4e415597f8836f1cdc6d9386493fd9fe4d0b9b81
yashith <yashith98@gmail.com>
2023-12-02 08:05:07 +0530

    Initial Commit

---
+++ src/main/java/com/gb/ATPLExamBulkInserter/AtplExamBulkInserterApplication.java
@@ -0,0 +1,13 @@
+package com.gb.ATPLExamBulkInserter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtplExamBulkInserterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtplExamBulkInserterApplication.class, args);
+	}
+
+}
